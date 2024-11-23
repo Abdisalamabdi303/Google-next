1e3cf9378c16fd6dd64ec9c8a6a899caac6d7b43
Abdisalamabdi303 <152012883+Abdisalamabdi303@users.noreply.github.com>
2024-11-23 11:44:02 +0300

    add pagination

--- src/components/ImageResults.jsx
+++ src/components/ImageResults.jsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import Pagination from "./Pagination";
 
 
 export default function ImageResults({results}) {
   return (
-    <div className="pb-24 mt-4 ">
+    <div className="sm:pb-24 pb-40 mt-4 ">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4">
             {results.items.map ((result)=>(
                 <div key={result.link} className="mb-8">
@@ -23,7 +24,8 @@ export default function ImageResults({results}) {
                     
                 </div>
             ))}
-        </div>       
+        </div>  
+        <div className="ml-6 "><Pagination/></div>     
     </div>
   )
 }
--- src/components/Webresults.jsx
+++ src/components/Webresults.jsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import Parser from 'html-react-parser'
+import Pagination from "./Pagination";
 
 export default function Webresults({results}) {
   return (
-    <div className="w-full mx-auto px-3 pb-24 sm:pl-[5%] md:pl-[14%] lg:pl-52">
+    <div className="w-full mx-auto px-3 sm:pb-24 pb-40 sm:pl-[5%] md:pl-[14%] lg:pl-52">
         <p className="text-gray-600 mt-3 text-sm mb-5">
             About {results.searchInformation?.formattedTotalResults} results
             ({results.searchInformation?.formattedSearchTime} seconds)
@@ -23,6 +24,7 @@ export default function Webresults({results}) {
                 </p>
             </div>
         ))}
+        <Pagination/>
     </div>
   )
 }
